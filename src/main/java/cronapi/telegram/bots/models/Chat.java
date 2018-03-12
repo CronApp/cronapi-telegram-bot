c2a84b624259bdb0e37601ebfeff85edfad5c67e
Michel Moreira <michel@michelmoreira.eti.br>
2018-03-12 10:16:25 -0300

    Typo

--- src/main/java/cronapi/telegram/bots/models/Chat.java
+++ src/main/java/cronapi/telegram/bots/models/Chat.java
@@ -6,7 +6,7 @@ public class Chat {
     private String title;
     private String firstName;
     private String lastName;
-    private String userName;
+    private String username;
     private Boolean allMembersAreAdministrators;
     private ChatPhoto photo;
     private String description;
@@ -55,12 +55,12 @@ public class Chat {
         this.lastName = lastName;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public Boolean getAllMembersAreAdministrators() {
